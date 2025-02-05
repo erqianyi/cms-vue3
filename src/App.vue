ab9ebf6770ef3c85db83cd6fdd5bfe598a1aa000
二千一 <wbzeqy@163.com>
2025-02-06 00:53:42 +0800

    feat: 增加代码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts"></script>
 
-<template>APP</template>
+<template>
+  <div>
+    <h2>APP</h2>
+    <router-link to="/main">主页</router-link>
+    <router-link to="/login">登录</router-link>
+    <router-view></router-view>
+  </div>
+</template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/main'
+    },
+    {
+      path: '/login',
+      component: () => import('../views/login/Login.vue')
+    },
+    {
+      path: '/main',
+      component: () => import('../views/main/Main.vue'),
+    }
+  ],
 })
 
 export default router
